90eb995c90b775c05da98630b6a19c4a780a67f5
Shinsei <murakami.shinsei.zn@gmail.com>
2025-04-25 09:28:19 +0900

    supabaseでgoogle認証

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,29 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { Alert, Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/src/components/EditScreenInfo';
 import { Text, View } from '@/src/components/Themed';
-import { useEffect } from 'react';
 import { supabase } from '@/src/lib/supabaseClient';
+import * as WebBrowser from 'expo-web-browser';
+import { useEffect } from 'react';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function TabOneScreen() {
   useEffect(() => {
-    const fetchData = async () => {
-      const { data, error } = await supabase.from('vocab').select('*');
-      if (error) {
-        console.error('Error fetching data:', error);
+    (async () => {
+      const {
+        data: { user },
+        error,
+      } = await supabase.auth.getUser();
+      if (user) {
+        console.log('ログイン中のユーザー:', user);
       }
-      if (data) {
-        console.log('Fetched data:', data);
-      }
-    };
-    fetchData();
+    })();
   }, []);
+  const handleLogin = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+    });
+
+    if (error) {
+      Alert.alert('ログイン失敗', error.message);
+    } else {
+      console.log('ログイン成功: ', data);
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Button title="Googleでログイン" onPress={handleLogin} />
     </View>
   );
 }
