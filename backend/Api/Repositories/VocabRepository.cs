336a9d2a410a9f0bdaaacdb9f09103af6a1fad16
Shinsei <murakami.shinsei.zn@gmail.com>
2025-05-02 09:15:48 +0900

    クラス初期化時に設定

--- backend/Api/Repositories/VocabRepository.cs
+++ backend/Api/Repositories/VocabRepository.cs
@@ -7,22 +7,25 @@ namespace Api.Repositories
   public class VocabRepository: IVocabRepository
   {
     private readonly HttpClient _httpClient;
+    private readonly string _supabaseUrl;
+    private readonly string _supabaseKey;
 
     public VocabRepository(HttpClient httpClient)
     {
       _httpClient = httpClient;
+      _supabaseUrl = Environment.GetEnvironmentVariable("SUPABASE_URL") 
+        ?? throw new ArgumentNullException(nameof(_supabaseUrl), "SUPABASE_URL is not set in environment variables.");
+      _supabaseKey = Environment.GetEnvironmentVariable("SUPABASE_API_KEY") 
+        ?? throw new ArgumentNullException(nameof(_supabaseKey), "SUPABASE_API_KEY is not set in environment variables.");
     }
 
     public async Task<List<Vocab>> GetVocabsAsync(string token)
     {
       try
       {
-        var supabaseUrl = Environment.GetEnvironmentVariable("SUPABASE_URL");
-        var supabaseKey = Environment.GetEnvironmentVariable("SUPABASE_API_KEY");
-
-        var request = new HttpRequestMessage(HttpMethod.Get, $"{supabaseUrl}/rest/v1/vocab");
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{_supabaseUrl}/rest/v1/vocab");
         request.Headers.Add("Authorization", $"Bearer {token}");
-        request.Headers.Add("apikey", supabaseKey);
+        request.Headers.Add("apikey", _supabaseKey);
 
         var response = await _httpClient.SendAsync(request);
         response.EnsureSuccessStatusCode();
